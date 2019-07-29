f7e1c2df8ff422fccab6bfbf19dd8ab9f3cd3cfc
Frédéric Jacques <gary.bob88@gmail.com>
2019-07-28 23:02:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from parametres_lecture import ParametresLecture
 from fenetre_principale import FenetrePrincipale
 
 if __name__ == "__main__" :
-    # Création des dossiers tmp et utl s'ils n'existent pas
+    # Création des dossiers 'tmp' et 'utl' s'ils n'existent pas
     os.makedirs("tmp", exist_ok=True)
     os.makedirs("utl", exist_ok=True)
     os.makedirs("utl/dictees", exist_ok=True)
@@ -14,12 +14,12 @@ if __name__ == "__main__" :
         try:
             parametre.charger("utl/parametres_lecture.val")
         except:
-            print("Une erreur est survenu lors du chargement des paramètres. Le contenu du fichier n'est pas valide. Les paramètres par défaut vont être chargés")
+            print("Une erreur est survenue lors du chargement des paramètres. Le contenu du fichier n'est pas valide. Les paramètres par défaut vont être chargés.")
             parametre = ParametresLecture()
 
     # Chager la dernière dictée non enregistrer
     dictee = Dictee()
-    if os.path.exists("utl/dictee_non_enregistrer.dct"):
+    if os.path.exists("utl/dictee_non_enregistree.dct"):
         try:
             dictee.charger("utl/dictee_non_enregistrer.dct")
         except:
@@ -29,7 +29,7 @@ if __name__ == "__main__" :
     fen = FenetrePrincipale(parametre, dictee)
     fen.mainloop()
 
-    # Réccupérer les options de lectures
+    # Récupérer les options de lectures
     parametre = ParametresLecture()
     parametre.nb_lecture_par_phrase = fen.nb_lecture_par_phrase()
     parametre.nb_mots_par_phrase_prononce = fen.nb_mots_par_phrases()
@@ -40,4 +40,4 @@ if __name__ == "__main__" :
     dictee = Dictee()
     dictee.titre = fen.TitreDictee.get("1.0", "end").replace('\n', '')
     dictee.text = fen.TextDictee.get("1.0", "end")
-    dictee.sauvegarder("utl/dictee_non_enregistrer.dct")
+    dictee.sauvegarder("utl/dictee_non_enregistree.dct")
